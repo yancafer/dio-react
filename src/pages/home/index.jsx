c180b8ba21405a912201eac415c1186d8f2dc2ee
Yanca Fernandes <yancafernandes2@gmail.com>
2024-06-22 19:44:50 -0500

    criando estrutura para o projeto

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Home = () => {
+  return (
+    <>
+      <h1>Home</h1>
+      <Link to="/login">Fazer</Link>
+    </>
+  );
+};
+
+
+export { Home }
